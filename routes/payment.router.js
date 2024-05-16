2b113a48c0f1ab51fe84602e43618890edb91cdc
andresmarchenaporras@gmail.com <Andres37DM>
2024-05-15 20:35:39 -0500

    pasarela de pagos

---
+++ routes/payment.router.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()  
+const createSession = require('../controllers/payment.controller.js')
+
+router.post('/create-checkout-session', createSession)
+router.get('/success', (req, res) => res.send('Success'))
+router.get('/cancel', (req, res) => res.send('Cancel'))
+
+module.exports = router
